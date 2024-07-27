05e211921c19e47c108baf299696b9825924b824
dxnn360 <muhammadyunus206@gmail.com>
2024-07-27 23:41:59 +0700

    v1

---
+++ server.js
@@ -0,0 +1,140 @@
+require('dotenv').config({path: './.env'});
+const express = require('express');
+const path = require('path');
+const {OAuthContext} = require('ibm-verify-sdk');
+const session = require('express-session');
+const storage = require('node-persist');
+
+const app = express();
+app.use(session({
+	secret: 'my-secret',
+  resave: true,
+  saveUninitialized: true,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'pug');
+app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'));
+
+
+storage.init();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+let config = {
+	tenantUrl : process.env.TENANT_URL,
+	clientId : process.env.CLIENT_ID,
+	clientSecret : process.env.CLIENT_SECRET,
+	flowType : process.env.FLOW_TYPE,
+	scope : process.env.SCOPE
+};
+
+let deviceFlow = new OAuthContext(config);
+
+
+const verifyToken = async(req, res, next) => {
+
+	const token = await storage.getItem(req.sessionID);
+	if(token) {
+		req.session.token = token;
+		next();
+	} else {
+		res.redirect('/');
+	}
+
+}
+
+app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.get('/authenticated', verifyToken, (req, res) => {
+	console.log('======== Requesting userInfo claims using valid token');
+		deviceFlow.userInfo(req.session.token)
+		.then((response) => {
+			res.render('authenticated', {message: 'successfully authenticated', userInfo: response.response});
+		}).catch((err) => {
+			res.send(err);
+		});
+});
+
+app.get('/halo', verifyToken, (req, res) => {
+	console.log('======== Requesting userInfo claims using valid token');
+		deviceFlow.userInfo(req.session.token)
+		.then((response) => {
+			res.render('halo', {message: 'successfully authenticated', userInfo: response.response});
+		}).catch((err) => {
+			res.send(err);
+		});
+});
+
+app.get('/authorize', (req, res) => {
+	console.log('======== Calling device_authorization endpoint')
+		deviceFlow.authorize()
+			.then((response) => {
+				console.log('======== API response: ', response);
+				res.render('authorize', {
+					userCode: response.user_code,
+					verificationUri: response.verification_uri,
+					qrCode: response.verification_uri_complete_qrcode
+				}, pollToken(response.device_code, req.sessionID));
+			})
+			.catch((error)=> {
+				console.log("========= API error trying to call device_authorization endpoint:", error);
+			});
+});
+
+app.get('/logout', async(req, res) => {
+	if(!req.session.token){
+		console.log('======== No token stored in session')
+		res.redirect('/');
+		return;
+	}
+	console.log('======== Attempting to revoke access_token');
+	deviceFlow.revokeToken(req.session.token, 'access_token')
+	.then(async() => {
+		console.log('======== Successfully revoked access token');
+		await storage.removeItem(req.sessionID);
+		console.log('======== Removing token session from storage');
+		res.redirect(302, '/')
+	})
+	.catch((error) => {
+		console.log('========= Token revocation error: ', error)
+	})
+})
+
+io.on('connection', function(socket) {
+	console.log('node client connected');
+	socket.on('disconnect', function() {
+		console.log('client disconnected');
+	});
+});
+
+
+function pollToken(device_code, sessionID ) {
+	let deviceCode = device_code;
+	let timeoutInterval = 5000;
+	console.log("========= Polling token api");
+	deviceFlow.pollTokenApi(deviceCode, timeoutInterval)
+	.then(async(response) => {
+		console.log('========= Token response object: ', response);
+		try {
+			await storage.setItem(sessionID, {...response});
+			await io.emit('success', {auth: '/authenticated'});
+		} catch (error) {
+			console.log('========= Error setting storage for session');
+			return error;
+		}
+	})
+	.catch((err) => {
+		console.log('========= Polling error: ', err);
+	});
+}
+
+http.listen(3000, () => {
+	console.log('Server started');
+	console.log('Navigate to http://localhost:3000');
+});
